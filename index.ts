138c6650eff59f5cd3dd9f5715c7cef2baba5f8d
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2021-01-30 19:12:25 +0200

    clean

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
 import express from 'express';
-import fs from 'fs';
 import { getRestaurantsByLatAndLng } from './controllers/RestaurantController';
 
 const PORT = 8000;
-
-const data = fs.readFileSync('./data/restaurants.json', 'utf-8');
 const app = express();
 
 app.get('/discovery', getRestaurantsByLatAndLng)
